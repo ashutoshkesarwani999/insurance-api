6684dd2757f7e70a810629e00d64a40fb7f3df5c
akesarwani360 <ashutoshkesarwani999@gmail.com>
2024-10-14 12:58:47 +0100

    feat: Added exception handling

--- api/v1/insurances/insurances.py
+++ api/v1/insurances/insurances.py
@@ -1,92 +1,97 @@
-import json
-import redis
-from fastapi import APIRouter, Depends, Body
+from fastapi import APIRouter, Depends, HTTPException
+from typing import List
 from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import HTTPException
 
 from core.database.session import generate_async_session
-from core.cache.redis import get_redis_client
 from app.controllers.insurance import get_all_policies_async, get_one_policy_async
-from app.models.requests.insurance import RequestModel
 from app.models.responses.insurance import (
     InsuranceResponse,
     sampleInsuranceResponse,
     sampleListInsuranceResponse,
 )
 from app.logger.logger import logger
+from app.service.presigned_url import generate_presigned_url
 
 insurance_router = APIRouter()
 
 
 @insurance_router.get(
     "/{customer_id}",
-    response_model=list[InsuranceResponse],
+    response_model=List[InsuranceResponse],
     summary="Retrieve all insurances",
     description="Fetch a list of all available insurances.",
     tags=["Insurance"],
     responses=sampleListInsuranceResponse,
 )
 async def get_insurances(
-    customer_id: RequestModel,
-    async_session: AsyncSession = Depends(generate_async_session),
-    redis: redis = Depends(get_redis_client),
-) -> list[InsuranceResponse]:
-        cache_key = f"all_policies:{customer_id}"
-
-        # Try to get data from cache
-        cached_data = redis.get(cache_key)
-        if cached_data:
-            logger.info(f"Cache hit for all policies of customer {customer_id}")
-            return json.loads(cached_data)
-        insurance = await get_all_policies_async(
-            async_session=async_session,
-            customer_id=customer_id
+    customer_id: int, async_session: AsyncSession = Depends(generate_async_session)
+) -> List[InsuranceResponse]:
+    try:
+        insurance_list: List[InsuranceResponse] = await get_all_policies_async(
+            async_session=async_session, customer_id=customer_id
         )
 
-        redis.setex(cache_key, 3600, json.dumps([ins.dict() for ins in insurance]))
-        return insurance
+        if not insurance_list:
+            raise HTTPException(status_code=404, detail="Insurance not found")
+
+        processed_insurance_list = [
+            InsuranceResponse(
+                insurance_id=insurance.insurance_id,
+                customer_policy_url=generate_presigned_url(
+                    insurance.customer_policy_url
+                ),
+                customer_id=insurance.customer_id,
+            )
+            for insurance in insurance_list
+        ]
+        return processed_insurance_list
+    except ValueError as e:
+        logger.warning(str(e), exc_info=True)
+        raise HTTPException(status_code=400, detail=str(e))
+    except HTTPException as e:
+        logger.warning(f"HTTPException occurred: {e.detail}", exc_info=False)
+        raise e
+    except Exception as e:
+        logger.error(str(e), exc_info=True)
+        raise HTTPException(status_code=500, detail="An unexpected error occurred")
 
 
 @insurance_router.get(
     "/get_policy/{customer_id}/{insurance_id}",
-    response_model=InsuranceResponse|dict,
+    response_model=InsuranceResponse | dict,
     summary="Retrieve a specific insurance",
     description="Fetch details of a specific insurance by its UUID.",
     tags=["Insurance"],
     responses=sampleInsuranceResponse,
 )
 async def get_insurance(
-    customer_id: RequestModel, insurance_id: RequestModel,
+    customer_id: int,
+    insurance_id: int,
     async_session: AsyncSession = Depends(generate_async_session),
-    redis: redis = Depends(get_redis_client)
-) -> InsuranceResponse|dict:
-    cache_key = f"policy:{customer_id}:{insurance_id}"
+) -> InsuranceResponse:
+    try:
+        insurance = await get_one_policy_async(
+            async_session=async_session,
+            insurance_id=int(insurance_id),
+            customer_id=int(customer_id),
+        )
+        if not insurance:
+            raise HTTPException(status_code=404, detail="Insurance not found")
+        processed_insurance = InsuranceResponse(
+            insurance_id=insurance.insurance_id,
+            customer_policy_url=generate_presigned_url(insurance.customer_policy_url),
+            customer_id=insurance.customer_id,
+        )
+        return processed_insurance
 
-    cached_data = redis.get(cache_key)
-    if cached_data:
-        logger.info(f"Cache hit for policy {insurance_id} of customer {customer_id}")
-        return json.loads(cached_data)
+    except ValueError as e:
+        logger.warning(str(e), exc_info=True)
+        raise HTTPException(status_code=400, detail=str(e))
 
-    insurance = await get_one_policy_async(
-        async_session=async_session,
-        insurance_id=int(insurance_id),
-        customer_id=int(customer_id)
-    )
-    redis.setex(cache_key, 3600, json.dumps(insurance.dict() if isinstance(insurance, InsuranceResponse) else insurance))
-    return insurance
-
-
-@insurance_router.post(
-    "/invalidate_cache",
-    summary="Invalidate cache for a customer",
-    description="Invalidate all cached data for a specific customer.",
-    tags=["Insurance"])
-async def invalidate_cache(
-    customer_id: str,
-    redis: redis = Depends(get_redis_client),
-):
-    redis.delete(f"all_policies:{customer_id}")
-
-    for key in redis.scan_iter(f"policy:{customer_id}:*"):
-        redis.delete(key)
-
-    return {"message": f"Cache invalidated for customer {customer_id}"}
+    except HTTPException as e:
+        logger.warning(f"HTTPException occurred: {e.detail}", exc_info=False)
+        raise e
+    except Exception as e:
+        logger.error(str(e), exc_info=True)
+        raise HTTPException(status_code=500, detail="An unexpected error occurred")
