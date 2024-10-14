3a46fd87fd89d4fbc6c39564a7d0690837256551
akesarwani360 <ashutoshkesarwani999@gmail.com>
2024-10-14 03:43:43 +0100

    refactor: refactored main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import uvicorn
+import argparse
 
 from core.config import config
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run the FastAPI server")
+    parser.add_argument('--port', type=int, default=8000)
+    args = parser.parse_args()
     uvicorn.run(
         app="core.server:app",
+        host="0.0.0.0",
+        port=args.port,
         reload=True if config.ENVIRONMENT != "production" else False,
         workers=1,
     )
