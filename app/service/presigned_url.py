3de9dca7b0f3230079d05697edfc4342df31729b
akesarwani360 <ashutoshkesarwani999@gmail.com>
2024-10-14 14:52:20 +0100

    fix: added access key retrieval from env variable

--- app/service/presigned_url.py
+++ app/service/presigned_url.py
@@ -1,8 +1,15 @@
 import os
 import boto3
 from botocore.exceptions import ClientError
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError, ClientError
 
 from app.logger.logger import logger
+
+
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+aws_region = os.getenv("AWS_REGION")
+
 
 def generate_presigned_url(s3_url:str, expiration:int=3600):
     """
@@ -21,10 +28,19 @@ def generate_presigned_url(s3_url:str, expiration:int=3600):
         return None
 
     # Create a boto3 client
-    s3_client = boto3.client(
+    try:
+        s3_client = boto3.client(
         's3',
-        region_name=os.environ.get('AWS_REGION','eu-west-1')
+        region_name=os.environ.get('AWS_REGION','eu-west-1'),
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key,
     )
+    except (NoCredentialsError, PartialCredentialsError) as e:
+        logger.error(f"Error in AWS credentials: {e}",exc_info=True)
+        raise e
+    except Exception as e:
+        logger.error(f"An error occurred while creating the S3 client: {e}",exc_info=True)
+        raise e
 
     try:
         response = s3_client.generate_presigned_url('get_object',
