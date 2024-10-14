b92977aa558b0dd7b68337b1a2e6d012de30e06a
akesarwani360 <ashutoshkesarwani999@gmail.com>
2024-10-14 03:47:43 +0100

    feat: Added db config

--- core/database/session.py
+++ core/database/session.py
@@ -1,79 +1,50 @@
-from contextvars import ContextVar, Token
-from typing import Union
+# ------------------------------------------------------------------------------
+# Database
+# ------------------------------------------------------------------------------
 
-from sqlalchemy.ext.asyncio import (
-    AsyncSession,
-    async_scoped_session,
-    create_async_engine,
-)
-from sqlalchemy.orm import Session, declarative_base, sessionmaker
-from sqlalchemy.sql.expression import Delete, Insert, Update
+import logging
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import text
 
 from core.config import config
 
-session_context: ContextVar[str] = ContextVar("session_context")
+logger = logging.getLogger("uvicorn")
+logging.getLogger("sqlalchemy.engine.Engine").handlers = logger.handlers
 
+async_engine = create_async_engine(
+    config.POSTGRES_URL,
+    echo=True
+)
 
-def get_session_context() -> str:
-    return session_context.get()
-
-
-def set_session_context(session_id: str) -> Token:
-    return session_context.set(session_id)
-
-
-def reset_session_context(context: Token) -> None:
-    session_context.reset(context)
-
-# # Here's an example of how these functions might be used:
-
-
-# # Set the session context for a new request
-# session_id = "abc123"
-# token = set_session_context(session_id)
-
-# # Perform operations that require the session context
-# current_session = get_session_context()  # Returns "abc123"
-
-# # Reset the session context after the request is completed
-# reset_session_context(token)
-engines = {
-    "writer": create_async_engine(str(config.POSTGRES_URL), pool_recycle=3600),
-    "reader": create_async_engine(str(config.POSTGRES_URL), pool_recycle=3600),
-}
-
-
-class RoutingSession(Session):
-    def get_bind(self, mapper=None, clause=None, **kwargs):
-        if self._flushing or isinstance(clause, (Update, Delete, Insert)):
-            return engines["writer"].sync_engine
-        return engines["reader"].sync_engine
-
-
-async_session_factory = sessionmaker(
+async_sessionmaker = sessionmaker(
+    bind=async_engine,
     class_=AsyncSession,
-    sync_session_class=RoutingSession,
-    expire_on_commit=False,
+    autocommit=False,
+    autoflush=False
 )
-
-session: Union[AsyncSession, async_scoped_session] = async_scoped_session(
-    session_factory=async_session_factory,
-    scopefunc=get_session_context,
-)
-
-
-async def get_session():
-    """
-    Get the database session.
-    This can be used for dependency injection.
-
-    :return: The database session.
-    """
-    print("Inside session")
-    try:
-        yield session
-    finally:
-        await session.close()
-
 
 Base = declarative_base()
+
+
+async def generate_async_session() -> AsyncGenerator[AsyncSession, None]:
+    """
+    Dependency function to yield an async SQLAlchemy ORM session.
+
+    Yields:
+        AsyncSession: An instance of an async SQLAlchemy ORM session.
+    """
+    async with async_sessionmaker() as async_session:
+        yield async_session
+
+
+async def test_connection():
+    async with async_sessionmaker() as session:
+        try:
+            # Try to execute a simple query
+            result = await session.execute(text("SELECT 1"))
+            return result.scalar() == 1
+        except Exception as e:
+            print(f"Database connection failed: {e}")
+            return False
